439c4b06192880279f417ce2389dd4f9cf0b0ffd
daxthin <nicolasdga.dev@gmail.com>
2023-11-18 19:54:06 -0300

    hero subtitle fixed

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -6,10 +6,10 @@ export const Hero: React.FC<IHeroProps> = ({title= "Sample title", description, 
     const isTabletOrMobile = useMediaQuery({ query: '(max-width: 800px)' })
     return (
         <section id="hero" className="flex flex-col justify-center items-center gap-8 w-full relative pb-32">
-        <div className="flex flex-col gap-11 mt-32">
+        <div className="flex flex-col justify-center items-center gap-11 mt-32">
           <h3 className="text-black font-bold text-2xl text-center">{subtitle}</h3>
           <h1 className="text-black font-bold text-4xl md:text-6xl text-center">{title}</h1>
-          <div className="m-auto mt-4 mb-4 w-[350px] md:w-[960px]">
+          <div className={`mt-4 mb-4 ${isTabletOrMobile ? 'w-[300px]' : 'w-[900px]'}`}>
             <p className="text-black font-medium md:text-2xl text-center">
                 {
                     description
